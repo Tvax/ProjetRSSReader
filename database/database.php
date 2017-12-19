5377cc6ec3237db927b4d35c96315663c44c02af
Tvax <19556689+Tvax@users.noreply.github.com>
2017-12-19 15:11:05 +0100

    Update database.php

--- database/database.php
+++ database/database.php
@@ -14,7 +14,7 @@ class Database{
 
   private function connect(){
 //    $this->password = 'JXXfhRc7EL';
-    $this->password = 'JXXfhRc7EL';
+    $this->password = getenv('DBPASSWD_ENV');
     $this->conn = new mysqli($this->servername, $this->username, $this->password, $this->dbname);
     // Check connection
     if ($this->conn->connect_error) {
